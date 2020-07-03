3f890c29fd789a3a66bc4629951e4df05fd344b2
Fani-19 <estefbd@hotmail.com>
2020-07-03 20:16:55 +0200

    Secciones y datos

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
-var nombre=promt("Introduce tu nombre: ");
-document.getElementById("nombre").innerText=nombre;
+$(document).ready(()=>{
+    var nombre=prompt("Introduce tu nombre: ");
+    $("#nombre").text(nombre)
+
+    $("#btnFormacion").click(()=>{
+        $("#formacion").show();
+        $("#experiencia").hide();
+        $("#otros").hide();
+    });
+    $("#btnExperiencia").click(()=>{
+        $("#experiencia").show();
+      $("#formacion").hide();
+      $("#otros").hide();
+    });
+    $("#btnOtros").click(()=>{
+        $("#experiencia").hide();
+      $("#formacion").hide();
+      $("#otros").show();
+    });
+})
